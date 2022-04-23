96bdcbe5aee8abaffe1c1a878ae773a1e947b4b5
Ahmet Oztemiz <ahmetoztemiz@gmail.com>
2022-04-23 13:34:35 +0300

    Add: initialize project

---
+++ eKar/eKarApp.swift
@@ -0,0 +1,17 @@
+//
+//  eKarApp.swift
+//  eKar
+//
+//  Created by Ahmet Öztemiz on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct eKarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
